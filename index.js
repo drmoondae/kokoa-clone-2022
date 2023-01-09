b9d91417c9d269851404f7c00fe4d63799ef50b9
drmoondae <116715814+drmoondae@users.noreply.github.com>
2023-01-09 17:18:13 +0900

    유데미 HTML&CSS 연습

---
+++ index.js
@@ -0,0 +1,7 @@
+let greetingText = "Hi, i'm Max!";
+        let phoneNumber = 010-5555-1232;
+        alert(greetingText);
+        alert(greetingText+'!!!');
+        greetingText = 'thank you, girl';
+        alert(greetingText);
+        alert('My phone number is ' + phoneNumber);
